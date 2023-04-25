4e4bf1bc7e18634eca5acef09dd45f84103ed4e6
eren okur <32430029+erenokur@users.noreply.github.com>
2023-04-25 23:33:44 +0300

    translation added and store screen has been made.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import { Inter } from "next/font/google";
-import "@/styles/home.page.css";
-const inter = Inter({ subsets: ["latin"] });
-
+import SuspenseWrapper from "../components/SuspenseWrapper";
+import WelcomePage from "../components/WelcomePage";
 export default function Home() {
-  return <div className={"scene"}></div>;
+  return (
+    <SuspenseWrapper>
+      <WelcomePage />
+    </SuspenseWrapper>
+  );
 }
