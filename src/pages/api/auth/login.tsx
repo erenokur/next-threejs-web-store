ccbfc1f3a5966f39242ce710ed076c4177faf65a
eren okur <32430029+erenokur@users.noreply.github.com>
2023-04-29 15:54:29 +0300

    backend login problem solved

--- src/pages/api/auth/login.tsx
+++ src/pages/api/auth/login.tsx
@@ -1,5 +1,5 @@
 import Cors from "cors";
-import { PrismaClient, UserWhereUniqueInput } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
@@ -20,26 +20,31 @@ export default async function handleLogin(
         resolve(true);
       });
     });
+    if (req.method !== "POST") {
+      res.status(405).json({ message: "Method not allowed" });
+      return;
+    }
+
     const { email, password } = req.body;
-
-    const userWhere: UserWhereUniqueInput = { email };
-    const user = await prisma.user.findUnique({ where: { email } });
-
+    console.log(email, password);
+    const user = await prisma.user.findUnique({ where: { email: email } });
     if (!user) {
       throw new Error("Invalid email or password");
     }
-
     const isMatch = await bcrypt.compare(password, user.password);
-
     if (!isMatch) {
       throw new Error("Invalid email or password");
     }
-
     const token = jwt.sign({ userId: user.id }, process.env.API_KEY || "", {
-      expiresIn: "1h",
+      expiresIn: "48h",
     });
 
-    res.status(200).json({ message: "Logged in successfully", token });
+    res.status(200).json({
+      message: "Logged in successfully",
+      token,
+      username: user.username,
+      role: user.username,
+    });
   } catch (error: any) {
     res.status(401).json({ message: error.message });
   }
