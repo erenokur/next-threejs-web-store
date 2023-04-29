dcbf4688e1253212e56e70e8ba2f71ac1d0f5c3d
eren okur <32430029+erenokur@users.noreply.github.com>
2023-04-29 13:51:05 +0300

    thunder activity added to the gitignore

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,7 +1,5 @@
-// pages/api/login.ts
-
 import Cors from "cors";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserWhereUniqueInput } from "@prisma/client";
 import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
@@ -11,7 +9,10 @@ const cors = Cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
 
-const handleLogin = async (req: NextApiRequest, res: NextApiResponse) => {
+export default async function handleLogin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     await new Promise((resolve, reject) => {
       cors(req, res, (err) => {
@@ -21,6 +22,7 @@ const handleLogin = async (req: NextApiRequest, res: NextApiResponse) => {
     });
     const { email, password } = req.body;
 
+    const userWhere: UserWhereUniqueInput = { email };
     const user = await prisma.user.findUnique({ where: { email } });
 
     if (!user) {
@@ -41,6 +43,4 @@ const handleLogin = async (req: NextApiRequest, res: NextApiResponse) => {
   } catch (error: any) {
     res.status(401).json({ message: error.message });
   }
-};
-
-export default handleLogin;
+}
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -1,46 +1,59 @@
-// pages/api/login.ts
-
 import Cors from "cors";
+import { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 const cors = Cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 });
-
-const handleLogin = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    await new Promise((resolve, reject) => {
-      cors(req, res, (err) => {
-        if (err) return reject(err);
-        resolve(true);
-      });
+export default async function handleRegister(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await new Promise((resolve, reject) => {
+    cors(req, res, (err) => {
+      if (err) return reject(err);
+      resolve(true);
     });
-    const { email, password } = req.body;
+  });
 
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method not allowed" });
+    return;
+  }
+
+  try {
+    const { username, email, password, role } = req.body;
+
+    // Check if email is already registered
     const user = await prisma.user.findUnique({ where: { email } });
 
-    if (!user) {
-      throw new Error("Invalid email or password");
+    if (user) {
+      res.status(400).json({ message: "Email already registered" });
+      return;
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (!isMatch) {
-      throw new Error("Invalid email or password");
-    }
-
-    const token = jwt.sign({ userId: user.id }, process.env.API_KEY || "", {
-      expiresIn: "1h",
+    // Create the user
+    const newUser = await prisma.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedPassword,
+        role: role,
+      },
     });
 
-    res.status(200).json({ message: "Logged in successfully", token });
-  } catch (error: any) {
-    res.status(401).json({ message: error.message });
-  }
-};
+    // Generate a JWT token
+    const token = jwt.sign({ userId: newUser.id }, process.env.API_KEY || "");
 
-export default handleLogin;
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
